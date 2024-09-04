1e0bfaf08460740bbb00789777a239fab824c2f0
Work <work@Massimos-MacBook-Pro.local>
2024-09-04 12:41:20 -0300

    Font fixing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/home";
+import './fonts.css';
+
 
 function App() {
   return (
